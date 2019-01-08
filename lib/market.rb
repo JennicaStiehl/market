581f743364ee7dccc8fad00564c544229c839704
JennicaStiehl <jennica.stiehl@gmail.com>
2019-01-08 10:35:14 -0700

    adds iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -23,4 +23,43 @@ class Market
     end
   end
 
+  def sorted_item_list
+    sorted = []
+    @vendors.each do |vendor|
+      vendor.inventory.keys.map do |item|
+        sorted << item
+      end
+    end
+    sorted.uniq.sort
+  end
+
+  def total_inventory
+    total = Hash.new(0)
+    total_amount = 0
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, amount|
+        if total.keys.include?(item)
+          total_amount += amount
+          total[item] += total_amount
+        elsif !total.keys.include?(item)
+          total[item] = amount
+        end
+      end
+    end
+    total
+    #----------------------------------------#
+    # new_amount = 0
+    # @vendors.each do |vendor|
+    #   vendor.inventory.each do |item, amount|
+    #     if vendor.check_stock(item) >= 0
+    #       new_amount = amount += vendor.check_stock(item)
+    #       total[item] = new_amount
+    #     else
+    #       total[item] = amount
+    #     end
+    #   end
+    # end
+    # total
+  end
+
 end
